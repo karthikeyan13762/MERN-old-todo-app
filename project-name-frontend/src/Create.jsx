6a830b1ecc5255a6e88f130108ffaed20708ee2a
unknown <karthikeya3762@gmail.com>
2024-09-03 23:13:53 +0530

    old toto app MERN

---
+++ project-name-frontend/src/Create.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Create({ setTodo, editTask, handleUpdate, setEditTask }) {
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    if (editTask) {
+      setTask(editTask.task);
+    } else {
+      setTask("");
+    }
+  }, [editTask]);
+
+  function handleClick() {
+    if (!task.trim()) {
+      return;
+    }
+
+    if (editTask) {
+      handleUpdate({ ...editTask, task });
+      setEditTask(null);
+    } else {
+      axios
+        .post("https://todo-backend-mj09.onrender.com/add", { task })
+        .then((res) => {
+          setTodo((prev) => [...prev, res.data]);
+          setTask("");
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleClick();
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="create_form_input"
+          name="task"
+          placeholder="Enter a task name"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          required
+        />
+        <button type="submit" id="create_form_button">
+          {editTask ? "Update" : "Add"}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Create;
